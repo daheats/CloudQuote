92c0eba5e60f2a4c8a934b37c079d26dfcbbb8b1
Heather Luna <heats@Heathers-MacBook-Pro.local>
2025-04-25 21:23:33 -0400

    Setting up Database schema.

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 from djmoney.models.fields import MoneyField
 
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    product_price = MoneyField(max_digits=14, decimal_places=2, default_currency="USD")
+
+    def __str__(self):
+        return self.product_name
+
+
